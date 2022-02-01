044fcea828fab6fb1ad32e0f324c12586af5e16c
Ilhomjon Mamadjonov <Ilhomjonmamadjonov1907@gmail.com>
2022-02-01 12:10:09 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:friendly_eats/pages/home_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Friedly Eats App",
+      home: FriendlyEatsApp(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+
+class FriendlyEatsApp extends StatefulWidget {
+  static const String id = "/main_page";
+
+  @override
+  _FriendlyEatsAppState createState() => _FriendlyEatsAppState();
+}
+
+class _FriendlyEatsAppState extends State<FriendlyEatsApp> {
+  List<String> images = [
+    "assets/images/r1.jpg",
+    "assets/images/r2.jpg",
+    "assets/images/r3.jpg",
+    "assets/images/r4.jpg",
+    " assets/images/r5.jpg",
+    " assets/images/r6.jpg",
+    "assets/images/r7.jpg",
+    " assets/images/r8.jpg",
+    " assets/images/r9.jpg",
+    " assets/images/r10.jpg",
+    " assets/images/r11.jpg",
+    "assets/images/r12.jpg",
+    " assets/images/r13.jpg",
+    " assets/images/r14.jpg"
+  ];
+  List<String> namesfood = [
+    "Dinner SteakHouse",
+    "Fire Hyper",
+    "Deli Hyper",
+    "Dinner SteakHouse",
+    "Fire Hyper",
+    "Deli Hyper",
+    "Dinner SteakHouse",
+    "Fire Hyper",
+    "Deli Hyper",
+    "Dinner SteakHouse",
+    "Fire Hyper",
+    "Deli Hyper",
+    "Dinner SteakHouse",
+    "Fire Hyper",
+  ];
+  List<String> namesmeal = [
+    "Sushi",
+    "Brunch",
+    "Deli",
+    "Sushi",
+    "Brunch",
+    "Deli",
+    "Sushi",
+    "Brunch",
+    "Deli",
+    "Sushi",
+    "Brunch",
+    "Deli",
+    "Sushi",
+    "Brunch",
+  ];
+  List<String> places = [
+    "Seattle",
+    "Colorado Springs",
+    "Los Angeles",
+    "Omaha",
+    "Seattle",
+    "Colorado Springs",
+    "Los Angeles",
+    "Omaha",
+    "Seattle",
+    "Colorado Springs",
+    "Los Angeles",
+    "Omaha",
+    "Seattle",
+    "Colorado Springs",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bool usingMobileLayout = MediaQuery.of(context).size.shortestSide < 600;
+    return Scaffold(
+      ///Appbar
+      appBar: AppBar(
+        centerTitle: false,
+        leading: Icon(Icons.restaurant),
+        title: Text("FriendlyEats"),
+        bottom: PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.of(context).size.height * 0.06),
+          child: Container(
+            color: Colors.white,
+            margin: EdgeInsets.all(10),
+            child: Container(
+              padding: EdgeInsets.all(5),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Icon(
+                    Icons.filter_list,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "All Restaurants",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "by rating",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+
+      ///body LitView
+      body: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 5),
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: images.length,
+          itemBuilder: (context, index) {
+            return _itemBuilder(context, index);
+          },
+        ),
+      ),
+    );
+  }
+
+  ///list Builder
+  Container _itemBuilder(BuildContext context, int index) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 10, top: 10),
+
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.30,
+              margin: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(images[index]), fit: BoxFit.cover),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        namesfood[index],
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        "\$\$\$",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  rating(),
+                  Text("${namesmeal[index]} • ${places[index]}")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  ///rating
+  Row rating() {
+    return Row(
+      children: [
+        Icon(
+          Icons.star_sharp,
+          color: Colors.yellow,
+        ),
+        Icon(
+          Icons.star_sharp,
+          color: Colors.yellow,
+        ),
+        Icon(
+          Icons.star_sharp,
+          color: Colors.yellow,
+        ),
+        Icon(
+          Icons.star_half,
+          color: Colors.yellow,
+        ),
+        Icon(
+          Icons.star_border_sharp,
+          color: Colors.yellow,
+        ),
+      ],
+    );
+  }
+}
