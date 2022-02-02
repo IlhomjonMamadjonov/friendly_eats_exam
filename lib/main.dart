8e0c86d1904d50f8bc883b3d6f922b81491a336f
Ilhomjon Mamadjonov <Ilhomjonmamadjonov1907@gmail.com>
2022-02-03 01:28:05 +0500

    made responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:friendly_eats/pages/home_page.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations(
-  //     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
